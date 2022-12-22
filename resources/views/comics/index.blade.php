098020ed9cd919d5cf7b44ee030d3ded44bdfd51
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-12-22 16:35:11 +0100

    displayed the index page's data with table and add more details to the show.blade page

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -7,15 +7,21 @@
 @section('content')
     <h1>Dc Comics</h1>
     <a href="{{ route('comics.create') }}">Add new comic</a>
-    @foreach ($comicses as $comic)
-        <div class="card" style="width: 25rem;">
-            <div class="card-body">
-                <h3 class="card-title">{{ $comic->title }}</h3>
-                <h6 class="card-subtitle mb-2 text-muted">{{ $comic->series }}</h6>
-                <p class="card-text">Price{{ $comic->price }}</p>
-                <a href="{{ route('comics.show', $comic->id) }}" class="card-link">Card link</a>
-            </div>
+    <table class="table table-striped table-hover">
 
-        </div>
-    @endforeach
+        <tbody>
+            @foreach ($comicses as $comic)
+                <tr>
+                    <td>{{ $comic->title }}</td>
+                    <td>{{ $comic->series }}</td>
+                    <td>{{ $comic->price }}</td>
+                    <td>
+                        <a href="{{ route('comics.show', $comic->id) }}" class="card-link">Show comic</a>
+                        <a href="{{ route('comics.show', $comic->id) }}" class="card-link">Update comic</a>
+                        <a href="{{ route('comics.show', $comic->id) }}" class="card-link">Delete comic</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 @section('title', $comic->title)
 @section('content')
-    <p>{{ $comic->title }}</p>
+    <h2>Show comic details</h2>
+    <h4>{{ $comic->title }}</h4>
+    <p>{{ $comic->price }}</p>
+    <p>{{ $comic->series }}</p>
+    <p>{{ $comic->type }}</p>
+    <img class="mb-2" src="{{ $comic->thumb }}" alt="">
+    <p>{{ $comic->description }}</p>
 @endsection
